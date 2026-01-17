1b695895db80b0cda32688affa4655f428d1b4c6
Max Bickers <mbickers@umich.edu>
2026-01-17 15:51:12 -0500

    set up ios template

---
+++ ios/GooseArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooseArtApp.swift
+//  goose-art
+//
+//  Created by Max Bickers on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct GooseArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
