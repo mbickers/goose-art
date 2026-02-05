d58876b49994207847e93044da9cb0d73c9a2f9d
Max Bickers <mbickers@fastmail.com>
2026-02-04 23:12:55 -0500

    make inspect output nicer

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 import asyncio
+import json
 from typing import Annotated, Any
 from fastapi import FastAPI, Query, WebSocket, WebSocketDisconnect
+from fastapi.responses import Response
 
 from canvas_service import CanvasService
 from canvas_types import SequencedAction, Placement
@@ -70,7 +72,11 @@ async def canvas_handler(
 @app.get("/inspect")
 async def inspect_canvas(user_id: Annotated[str, Query(alias="userId")]):
     canvas = users[user_id]
-    return {
+    data = {
         "placements": [p.to_json() for p in canvas.placements],
         "greatestSeenDeviceSequenceNumbers": canvas.greatest_seen_device_sequence_numbers,
     }
+    return Response(
+        content=json.dumps(data, indent=2, ensure_ascii=False),
+        media_type="application/json; charset=utf-8",
+    )
